3d377ae74c2e368e6127cdf349d053ae1ee8915e
sabilanm <sabilanm1010@gmail.com>
2025-10-27 09:49:55 +0700

    update

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -6,15 +6,7 @@ import PrivateRoute from "./PrivateRoute.js";
 const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
 
 /***** Pages ****/
-const Starter = lazy(() => import("../views/Starter.js"));
-const About = lazy(() => import("../views/About.js"));
-const Alerts = lazy(() => import("../views/ui/Alerts"));
 const Badges = lazy(() => import("../views/ui/Badges"));
-const Buttons = lazy(() => import("../views/ui/Buttons"));
-const Cards = lazy(() => import("../views/ui/Cards"));
-const Grid = lazy(() => import("../views/ui/Grid"));
-const Tables = lazy(() => import("../views/ui/Tables"));
-const Forms = lazy(() => import("../views/ui/Forms"));
 const Breadcrumbs = lazy(() => import("../views/ui/Breadcrumbs"));
 const LoginLazy = lazy(() => import("../views/auth/Login"));
 const Dashboard = lazy(() => import("../views/dashboard/Dashboard.jsx"));
@@ -26,15 +18,7 @@ const FullLayoutRoutes = [
         path: "/",
         element: <FullLayout />,
         children: [
-            { path: "/starter", element: <PrivateRoute element={Starter} /> },
-            { path: "/about", element: <PrivateRoute element={About} /> },
-            { path: "/alerts", element: <PrivateRoute element={Alerts} /> },
             { path: "/badges", element: <PrivateRoute element={Badges} /> },
-            { path: "/buttons", element: <PrivateRoute element={Buttons} /> },
-            { path: "/cards", element: <PrivateRoute element={Cards} /> },
-            { path: "/grid", element: <PrivateRoute element={Grid} /> },
-            { path: "/table", element: <PrivateRoute element={Tables} /> },
-            { path: "/forms", element: <PrivateRoute element={Forms} /> },
             {
                 path: "/breadcrumbs",
                 element: <PrivateRoute element={Breadcrumbs} />,
