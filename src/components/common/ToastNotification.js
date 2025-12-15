02038914af723e8425671faa7d5f64fd83ea504a
AlbertusBaskoro <albertusbaskoro.g@gmail.com>
2025-12-15 11:44:09 +0700

    css toast

--- src/components/common/ToastNotification.js
+++ src/components/common/ToastNotification.js
@@ -1,48 +1,41 @@
 // src/components/common/Toast.js
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./ToastCss.css";
+
+const baseOptions = {
+    position: "top-right",
+    autoClose: 3000,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+};
 
 const ToastNotification = {
-    success: (message) => {
+    success: (message) =>
         toast.success(message, {
-            position: "top-right",
-            autoClose: 3000,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-        });
-    },
-    error: (message) => {
+            ...baseOptions,
+            className: "toast-success",
+        }),
+
+    error: (message) =>
         toast.error(message, {
-            position: "top-right",
-            autoClose: 3000,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-        });
-    },
-    info: (message) => {
+            ...baseOptions,
+            className: "toast-error",
+        }),
+
+    info: (message) =>
         toast.info(message, {
-            position: "top-right",
-            autoClose: 3000,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-        });
-    },
-    warning: (message) => {
+            ...baseOptions,
+            className: "toast-info",
+        }),
+
+    warning: (message) =>
         toast.warning(message, {
-            position: "top-right",
-            autoClose: 3000,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: true,
-            progress: undefined,
-        });
-    },
+            ...baseOptions,
+            className: "toast-warning",
+        }),
 };
 
 export default ToastNotification;
